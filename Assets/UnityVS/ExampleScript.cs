a428a07d48e2b043b90aecb61bfd8c7991c88b47
Raziaar <raziaar@gmail.com>
2014-10-09 18:45:12 -0500

    Testing new branch.

--- Assets/UnityVS/ExampleScript.cs
+++ Assets/UnityVS/ExampleScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ExampleScript : MonoBehaviour {
 
-	// This is our first script!
+	// Veg's comment deleted. Added Raz's comment on new branch.
 
     // And our second comment. Pushing this to test git in Visual Studio.
 }
