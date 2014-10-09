2fd8557f13aca88efc302225261395c9fe5d204a
Raziaar <raziaar@gmail.com>
2014-10-09 18:36:24 -0500

    Testing Git Push from Visual Studio.

--- Assets/UnityVS/ExampleScript.cs
+++ Assets/UnityVS/ExampleScript.cs
@@ -4,4 +4,6 @@ using System.Collections;
 public class ExampleScript : MonoBehaviour {
 
 	// This is our first script!
+
+    // And our second comment. Pushing this to test git in Visual Studio.
 }
