af3f2f5d495724e39b2c3099bbdfecee0e684b84
vegeta897 <vegeta897@gmail.com>
2014-10-09 19:56:44 -0400

    Test add update loop

--- Assets/UnityVS/ExampleScript.cs
+++ Assets/UnityVS/ExampleScript.cs
@@ -6,4 +6,9 @@ public class ExampleScript : MonoBehaviour {
 	// Veg's comment deleted. Added Raz's comment on new branch.
 
     // This is a comment in vegeta's branch.
+
+    void Update()
+    {
+        // Update loop
+    }
 }
