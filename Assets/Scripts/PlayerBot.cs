0368f5bed7ec7b9cd807f129b964f732afa43750
vegeta897 <vegeta897@gmail.com>
2014-10-12 21:20:39 -0400

    Added random rotation to wheels

--- Assets/Scripts/PlayerBot.cs
+++ Assets/Scripts/PlayerBot.cs
@@ -14,11 +14,11 @@ public class PlayerBot : MonoBehaviour {
     void Spawn() // Create a randomly sized box chassis and drop it in
     {
         chassis = GameObject.CreatePrimitive(PrimitiveType.Cube); // Set chassis to a cube
-        chassis.transform.parent = this.transform;
+        chassis.transform.parent = this.gameObject.transform;
         chassis.collider.material = (PhysicMaterial)Resources.Load("Materials/Metal"); // Set physics material to Metal
         chassis.transform.localScale = new Vector3(Random.Range(0.3F, 1F), Random.Range(0.3F, 1F), Random.Range(0.3F, 1F)); // Scale it randomly between 0.3 and 1 WxHxD
-        chassis.transform.position = new Vector3(0, chassis.transform.localScale.y / 2 + 0.5F, 0); // Position it 0.5 units above the ground
-        Rigidbody chassisRigidBody = chassis.AddComponent<Rigidbody>(); // Add the rigid body
+        this.gameObject.transform.position = new Vector3(0, chassis.transform.localScale.y / 2 + 0.5F, 0); // Position bot 0.5 units above the ground
+        Rigidbody chassisRigidBody = this.gameObject.AddComponent<Rigidbody>(); // Add the rigid body
         chassisRigidBody.mass = chassis.transform.localScale.x * chassis.transform.localScale.y * chassis.transform.localScale.z; // Set the mass to the volume
         AddWheels();
     }
@@ -28,17 +28,17 @@ public class PlayerBot : MonoBehaviour {
         int numberOfWheels = 6;
         int wheelCount = 0;
         RaycastHit hit;
-        Object wheel = Resources.Load("Prefabs/Wheel");
+        Object wheel = Resources.Load("Prefabs/Wheel"); // Load wheel prefab
         while (wheelCount < numberOfWheels)
         {
-            if (Physics.Linecast(Random.onUnitSphere * 2, chassis.transform.position, out hit))
+            if (Physics.Linecast(Random.onUnitSphere * 2, chassis.transform.position, out hit)) // Raycast from inside chassis to pick a random spot on surface
             {
-                GameObject thisWheel = (GameObject)GameObject.Instantiate(wheel, hit.point, Quaternion.identity);
+                GameObject thisWheel = (GameObject)GameObject.Instantiate(wheel, hit.point, Quaternion.identity); // Instantiate a wheel
                 float wheelDiameter = Random.Range(0.1F, 0.3F);
-                thisWheel.transform.localScale = new Vector3(wheelDiameter, Random.Range(0.02F, 0.08F), wheelDiameter);
-                thisWheel.transform.LookAt(hit.point + hit.normal);
-                thisWheel.transform.parent = chassis.transform;
-                // Issue: Parenting to the chassis makes it inherit the chassis scale, so weird things happen when you rotate the wheel
+                thisWheel.transform.localScale = new Vector3(wheelDiameter, Random.Range(0.02F, 0.08F), wheelDiameter); // Scale wheel randomly, keeping it circular
+                thisWheel.transform.LookAt(hit.point + hit.normal); // Align to face
+                thisWheel.transform.rotation = Quaternion.Euler(thisWheel.transform.rotation.eulerAngles + new Vector3(0, 0, Random.Range(0, 360F))); // Random rotation
+                thisWheel.transform.parent = this.gameObject.transform; // Set parent
                 wheelCount++;
             }
         }
