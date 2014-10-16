a221614ae3a6ee35c42794fbbf0f95bb20384984
vegeta897 <vegeta897@gmail.com>
2014-10-15 20:30:24 -0400

    Wheels no longer spawn inside eachother

--- Assets/Scripts/PlayerBot.cs
+++ Assets/Scripts/PlayerBot.cs
@@ -11,15 +11,16 @@ public class PlayerBot : MonoBehaviour {
 	// Use this for initialization
     void Start()
     {
-        Invoke("Spawn", 1F); // Spawn our bot after 1 second
+        Invoke("Spawn", 0.5F); // Spawn our bot after half a second
 	}
 
     void Spawn() // Create a randomly sized box chassis and drop it in
     {
         chassis = GameObject.CreatePrimitive(PrimitiveType.Cube); // Set chassis to a cube
         chassis.transform.parent = this.gameObject.transform;
+        chassis.layer = 8; // Set to PlayerBotChassis layer
         chassis.collider.material = (PhysicMaterial)Resources.Load("Materials/Metal"); // Set physics material to Metal
-        chassis.transform.localScale = new Vector3(Random.Range(0.3F, 1F), Random.Range(0.3F, 1F), Random.Range(0.3F, 1F)); // Scale it randomly between 0.3 and 1 WxHxD
+        chassis.transform.localScale = new Vector3(Random.Range(0.3F, 0.9F), Random.Range(0.3F, 0.8F), Random.Range(0.4F, 1.3F)); // Scale it randomly, tending toward a rectangular prism
         this.gameObject.transform.position = new Vector3(0, chassis.transform.localScale.y / 2 + 2F, 0); // Position bot 2 units above the ground
         botRigidBody = (Rigidbody)this.gameObject.GetComponent(typeof(Rigidbody));
         botRigidBody.mass = chassis.transform.localScale.x * chassis.transform.localScale.y * chassis.transform.localScale.z; // Set the mass to the volume
@@ -35,7 +36,7 @@ public class PlayerBot : MonoBehaviour {
         Object wheel = Resources.Load("Prefabs/Wheel"); // Load wheel prefab
         while (wheelCount < numberOfWheels)
         {
-            if (Physics.Linecast(this.gameObject.transform.position + Random.onUnitSphere * 2, this.gameObject.transform.position, out hit)) // Raycast toward center to pick random spot on surface
+            if (Physics.Linecast(this.gameObject.transform.position + Random.onUnitSphere * 2, this.gameObject.transform.position, out hit, 1 << 8)) // Raycast toward center to pick random spot on surface
             {
                 GameObject thisWheel = (GameObject)GameObject.Instantiate(wheel, hit.point, Quaternion.identity); // Instantiate a wheel
                 thisWheel.name = "Wheel_" + (wheelCount + 1); // Name it
@@ -45,14 +46,17 @@ public class PlayerBot : MonoBehaviour {
                 thisWheel.transform.LookAt(hit.point + hit.normal); // Align to face
                 float wheelDistance = (wheelWidth / 2) + 0.02F; // Define distance from chassis
                 thisWheel.transform.position = thisWheel.transform.position + hit.normal * wheelDistance; // Set distance
+                // If wheel is colliding with another wheel, delete it and try again
+                if (Physics.CheckSphere(thisWheel.transform.position, wheelDiameter, 1 << 9))
+                {
+                    Object.Destroy(thisWheel);
+                    continue;
+                }
+                thisWheel.layer = 9;
                 HingeJoint thisJoint = (HingeJoint)thisWheel.AddComponent<HingeJoint>(); // Create a hinge joint
                 thisJoint.axis = new Vector3(0, 0, -1); // Set joint axis (towards chassis)
                 thisJoint.anchor = new Vector3(0, 0, 0); // Set anchor to center of wheel // TODO: Change this to inside face of wheel
                 thisJoint.connectedBody = this.gameObject.GetComponent<Rigidbody>(); // Connect to rigid body
-                for (int i = 0; i < wheels.Count; i++) // Ignore collisions with other wheels
-                {
-                    Physics.IgnoreCollision(thisWheel.collider, wheels[i].collider);
-                }
                 wheels.Add(thisWheel); // Add to wheels array
                 wheelCount++; // Increment wheel count
             }
